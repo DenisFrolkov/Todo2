b928120a81114d576d8a111c5ace4c7b4bb1c6de
DenisFrolkov <99652468+DenisFrolkov@users.noreply.github.com>
2023-01-28 15:02:06 +0300

    Сделал Lab1

---
+++ Desktop/Registration.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Desktop
+{
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+    }
+}
